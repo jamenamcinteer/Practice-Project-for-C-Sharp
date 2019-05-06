dfd62f61dd57c82da40a46f79ef2512e921356ed
ndjamenamarmon <jamena@webdevabq.com>
2019-05-06 11:14:07 -0600

    initial commit

---
+++ PracticeProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] twoDimensionalArray = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+            _2DTo1D.Convert(twoDimensionalArray);
+        }
+    }
+}
---
+++ PracticeProject/_2DTo1D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    class _2DTo1D
+    {
+        // Convert a two dimensional array into a one dimensional array
+        public static int[] Convert(int[,] twoDimensionalArray)
+        {
+            int[] oneDimensionalArray = new int[twoDimensionalArray.GetLength(0) * twoDimensionalArray.GetLength(1)];
+            int index = 0;
+            for (int row = 0; row < twoDimensionalArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < twoDimensionalArray.GetLength(1); col++)
+                {
+                    oneDimensionalArray[index] = twoDimensionalArray[row, col];
+                    index++;
+                }
+            }
+            return oneDimensionalArray;
+        }
+    }
+}
